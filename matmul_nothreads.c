5f721d2107e52d02fbf3691a627c2b9e400bd8e9
YannosK <giannos2405@gmail.com>
2024-05-04 21:21:26 +0300

    simple

---
+++ matmul_nothreads.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define size 4
+
+void matrix_init(int *A, int dim);
+
+int main(void)
+{
+    int A[size][size];
+    int B[size][size];
+    int C[size][size];
+
+    return 0;
+}
+
+void matrix_init(int *A, int dim)
+{
+}
